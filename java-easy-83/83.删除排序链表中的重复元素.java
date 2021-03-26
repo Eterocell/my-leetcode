b065b49db59705782a3c1f9c11ad9146acd8f0e7
Eterocell <eterocell@outlook.com>
2021-03-26 20:26:07 +0800

    add 83

---
+++ java-easy-83/83.删除排序链表中的重复元素.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=java
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode ans = new ListNode(0, head);
+        ListNode curr = ans;
+
+        while (curr.next != null && curr.next.next != null) {
+            if (curr.next.val == curr.next.next.val) {
+                int x = curr.next.val;
+                while (curr.next.next != null && curr.next.next.val == x) {
+                    curr.next.next = curr.next.next.next;
+                }
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return ans.next;
+    }
+
+    // class ListNode {
+    //     int val;
+    //     ListNode next;
+
+    //     ListNode() {
+    //     }
+
+    //     ListNode(int val) {
+    //         this.val = val;
+    //     }
+
+    //     ListNode(int val, ListNode next) {
+    //         this.val = val;
+    //         this.next = next;
+    //     }
+    // }
+}
+// @lc code=end
+
